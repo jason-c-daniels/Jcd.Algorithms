00ada1753a7697fba0176d5d553639c4e93d6faa
Jason C. Daniels <jason.c.daniels@gmail.com>
2021-10-24 15:37:40 -0500

    simplified pre-checks when length == 1

--- Jcd.Algorithms/IsNumberExtension.cs
+++ Jcd.Algorithms/IsNumberExtension.cs
@@ -8,9 +8,12 @@ namespace Jcd.Algorithms
         public static bool IsNumber(this string s)
         {
             // take care of some well known cases.
-            if (s.Length < 1) return false;
-            if (s.Length == 1 && !s[0].IsDigit()) return false;
-            if (s.Length == 1 && s[0].IsDigit()) return true;
+            if (s == null || s.Length < 1) return false;
+            if (s.Length == 1)
+            {
+                return s[0].IsDigit();
+            }
+
             if (s.Length == 2 && s[0].IsPeriod() && s[1].IsDigit()) return true;
             if (s.Length == 2 && s[1].IsPeriod() && s[0].IsDigit()) return true;
             if (s.Length == 2 && s[0].IsSign() && s[1].IsDigit()) return true;
@@ -21,6 +24,7 @@ namespace Jcd.Algorithms
             if (s[^1].IsE()) return false;
             if (s[^1].IsSign()) return false;
 
+            // now parse the number
             var state = ParseState.Start;
             try
             {
@@ -38,58 +42,67 @@ namespace Jcd.Algorithms
 
         private static ParseState ProcessState(ParseState state, char c)
         {
-            if (!c.IsValid()) return ParseState.Error;
-            if (state == ParseState.Start)
+            // only process valid characters
+            if (c == '0' || c == '1' || c == '2' || c == '3' || c == '4' || c == '5' ||
+                c == '6' || c == '7' || c == '8' || c == '9' || c == '-' || c == '+' ||
+                c == 'e' || c == 'E' || c == '.')
             {
-                if (c.IsSign()) return ParseState.LeadingSign;
-                if (c.IsDigit()) return ParseState.InPrefix;
-                if (c.IsPeriod()) return ParseState.Period;
+                if (state == ParseState.Start)
+                {
+                    if (c.IsSign()) return ParseState.LeadingSign;
+                    if (c.IsDigit()) return ParseState.InPrefix;
+                    if (c.IsPeriod()) return ParseState.Period;
+                    return ParseState.Error;
+                }
+                else if (state == ParseState.LeadingSign)
+                {
+                    if (c.IsDigit()) return ParseState.InPrefix;
+                    if (c.IsPeriod()) return ParseState.Period;
+                    return (ParseState.Error);
+                }
+                else if (state == ParseState.InPrefix)
+                {
+                    if (c.IsDigit()) return ParseState.InPrefix;
+                    if (c.IsPeriod()) return ParseState.Period;
+                    if (c.IsE()) return ParseState.ExponentSymbol;
+                    return ParseState.Error;
+                }
+                else if (state == ParseState.Period)
+                {
+                    if (c.IsDigit()) return ParseState.InSuffix;
+                    if (c.IsE()) return ParseState.ExponentSymbol;
+                    return ParseState.Error;
+                }
+                else if (state == ParseState.InSuffix)
+                {
+                    if (c.IsDigit()) return ParseState.InSuffix;
+                    if (c.IsE()) return ParseState.ExponentSymbol;
+                    return ParseState.Error;
+                }
+                else if (state == ParseState.ExponentSymbol)
+                {
+                    if (c.IsSign()) return ParseState.ExponentSign;
+                    if (c.IsDigit()) return ParseState.InExponent;
+                    return ParseState.Error;
+                }
+                else if (state == ParseState.ExponentSign)
+                {
+                    if (c.IsDigit()) return ParseState.InExponent;
+                    return ParseState.Error;
+                }
+                else if (state == ParseState.InExponent)
+                {
+                    if (c.IsDigit()) return ParseState.InExponent;
+                    return ParseState.Error;
+                }
+                else if (state == ParseState.Final)
+                {
+                    throw new Exception("Final state reached but more text was pending parsing.");
+                }
+            }
+            else
+            {
                 return ParseState.Error;
-            }
-            else if (state == ParseState.LeadingSign)
-            {
-                if (c.IsDigit()) return ParseState.InPrefix;
-                if (c.IsPeriod()) return ParseState.Period;
-                return (ParseState.Error);
-            }
-            else if (state == ParseState.InPrefix)
-            {
-                if (c.IsDigit()) return ParseState.InPrefix;
-                if (c.IsPeriod()) return ParseState.Period;
-                if (c.IsE()) return ParseState.ExponentSymbol;
-                return ParseState.Error;
-            }
-            else if (state == ParseState.Period)
-            {
-                if (c.IsDigit()) return ParseState.InSuffix;
-                if (c.IsE()) return ParseState.ExponentSymbol;
-                return ParseState.Error;
-            }
-            else if (state == ParseState.InSuffix)
-            {
-                if (c.IsDigit()) return ParseState.InSuffix;
-                if (c.IsE()) return ParseState.ExponentSymbol;
-                return ParseState.Error;
-            }
-            else if (state == ParseState.ExponentSymbol)
-            {
-                if (c.IsSign()) return ParseState.ExponentSign;
-                if (c.IsDigit()) return ParseState.InExponent;
-                return ParseState.Error;
-            }
-            else if (state == ParseState.ExponentSign)
-            {
-                if (c.IsDigit()) return ParseState.InExponent;
-                return ParseState.Error;
-            }
-            else if (state == ParseState.InExponent)
-            {
-                if (c.IsDigit()) return ParseState.InExponent;
-                return ParseState.Error;
-            }
-            else if (state == ParseState.Final)
-            {
-                throw new Exception("Final state reached but more text was pending parsing.");
             }
 
             return state;
@@ -100,8 +113,7 @@ namespace Jcd.Algorithms
                                             c == '6' || c == '7' || c == '8' || c == '9' || c == '-' || c == '+' ||
                                             c == 'e' || c == 'E' || c == '.';
         
-        static bool IsDigit(this char c) => c == '0' || c == '1' || c == '2' || c == '3' || c == '4' || c == '5' ||
-                                            c == '6' || c == '7' || c == '8' || c == '9';
+        static bool IsDigit(this char c) => c == '0' || c == '1' || c == '2' || c == '3' || c == '4' || c == '5' || c == '6' || c == '7' || c == '8' || c == '9';
         static bool IsE(this char c) => c == 'e' || c == 'E';
         static bool IsPeriod(this char c) => c == '.';
         static bool IsSign(this char c) => c == '-' || c == '+';
